4fccb8c1beb7a321f10fa0bfbfbaefd6f92701ae
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2022-04-11 22:09:09 -0600

    feat: get stale files (#6)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="corganizeclient",
-    version="0.0.7",
+    version="0.0.8",
     author="Jaeseo Park",
     description="A Python client to interact with corganize server",
     long_description=long_description,
